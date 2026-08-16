b3eb3f326d3d5942bca368b9f59093950d4c5e4c
Adewale Orotayo <didymus7007@gmail.com>
2026-08-16 07:51:26 +0100

    feat(bespoke): move inventory into a products foldder

--- app/products/data.ts
+++ app/products/data.ts
@@ -16,7 +16,7 @@ export type Product = {
 };
 
 // Our Updated Store Inventory
-const INVENTORY: Product[] = [
+export const INVENTORY: Product[] = [
   {
     id: 1,
     name: "Noir Print Dinner Gown",
@@ -33,6 +33,28 @@ const INVENTORY: Product[] = [
     name: "Monochrome Brushstroke Co-ord",
     price: "£43.99",
     image: "/wifey-design-1.jpg",
+    category: "full_body",
+    status: "in_stock",
+    tagline: "In stock · Sourced from Lagos",
+    description:
+      "Make a bold statement with this striking two-piece matching set. Featuring a dramatic black-and-white abstract brushstroke print, this ensemble pairs a relaxed-fit, button-down shirt with flowing wide-leg trousers for an effortless, chic silhouette.",
+  },
+  {
+    id: 3,
+    name: "Stylish Aso-Oke Design Dress",
+    price: "£43.99",
+    image: "/aso-oke-dress.jpg",
+    category: "full_body",
+    status: "in_stock",
+    tagline: "In stock · Sourced from Lagos",
+    description:
+      "Make a bold statement with this striking two-piece matching set. Featuring a dramatic black-and-white abstract brushstroke print, this ensemble pairs a relaxed-fit, button-down shirt with flowing wide-leg trousers for an effortless, chic silhouette.",
+  },
+  {
+    id: 4,
+    name: "Aso Oke Top",
+    price: "£43.99",
+    image: "/aso-oke-top.jpg",
     category: "full_body",
     status: "in_stock",
     tagline: "In stock · Sourced from Lagos",
